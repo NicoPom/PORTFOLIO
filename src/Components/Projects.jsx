dbe92805a3ff27622a8d0a22c7974aa06b79106a
Nico Pom <niconp7@yahoo.fr>
2022-09-28 19:50:53 +0200

    project page

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,45 +1,42 @@
 import React from "react";
+import { videos } from "../videoData";
 
 export default function Projects() {
   // render the projects with a video and a link to the github repo
-  const videos = [
-    {
-      title: "Tic Tac Toe",
-      url: "video/scrim.mp4",
-      liveSite: "https://nicopom.github.io/Rock-Paper-Scissors/",
-      github: "https://github.com/NicoPom/Rock-Paper-Scissors",
-    },
-  ];
 
   function handleMouseOver(e) {
     e.target.play();
+    // cursor pointer
+    e.target.style.cursor = "pointer";
   }
 
   function handleMouseOut(e) {
     e.target.pause();
     e.target.currentTime = 0;
-    // display poster image
     e.target.load();
   }
 
   const videoElements = videos.map((video) => (
     <div className="project" key={video.title}>
       <h3>{video.title}</h3>
-      <video
-        className="project--video"
-        width="90%"
-        poster="img/profile_picture.png"
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-        muted
-      >
-        <source src={video.url} type="video/mp4" />
-      </video>
+      <a href="https://nicopom.github.io/Rock-Paper-Scissors/">
+        <video
+          className="project--video"
+          width="200px"
+          height="200px"
+          poster="img/profile_picture.png"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+          muted
+        >
+          <source src={video.url} type="video/mp4" />
+        </video>
+      </a>
       <div className="project--links">
-        <a href={video.liveSite} target="_blank" rel="noopener noreferrer">
+        <a href={video.liveSite} rel="noopener noreferrer">
           Live Site ↗
         </a>
-        <a href={video.github} target="_blank" rel="noopener noreferrer">
+        <a href={video.github} rel="noopener noreferrer">
           GitHub ↗
         </a>
       </div>
---
+++ src/videoData.jsx
@@ -0,0 +1,26 @@
+export const videos = [
+  {
+    title: "Tic Tac Toe",
+    url: "video/scrim.mp4",
+    liveSite: "https://nicopom.github.io/Rock-Paper-Scissors/",
+    github: "https://github.com/NicoPom/Rock-Paper-Scissors",
+  },
+  {
+    title: "Tic Tac Toe",
+    url: "video/scrim.mp4",
+    liveSite: "https://nicopom.github.io/Rock-Paper-Scissors/",
+    github: "https://github.com/NicoPom/Rock-Paper-Scissors",
+  },
+  {
+    title: "Tic Tac Toe",
+    url: "video/scrim.mp4",
+    liveSite: "https://nicopom.github.io/Rock-Paper-Scissors/",
+    github: "https://github.com/NicoPom/Rock-Paper-Scissors",
+  },
+  {
+    title: "Tic Tac Toe",
+    url: "video/scrim.mp4",
+    liveSite: "https://nicopom.github.io/Rock-Paper-Scissors/",
+    github: "https://github.com/NicoPom/Rock-Paper-Scissors",
+  },
+];
