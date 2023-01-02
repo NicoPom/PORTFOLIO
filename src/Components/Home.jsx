5e4d40d06916713a23e82ba8a6d0692aac993d15
Nico Pom <niconp7@yahoo.fr>
2023-01-02 21:28:58 +0100

    redesign announcement

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="container home--container">
-      <h1>Nicolas POMARES</h1>
-
-      <h3>Frontend Developer</h3>
-    </div>
+    <>
+      <div className="container home--container">
+        <h1>Nicolas POMARES</h1>
+        <h3>Frontend Developer</h3>
+        <p>Actually in the process of redesigning this portfolio</p>
+      </div>
+    </>
   );
 }
